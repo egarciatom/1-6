48f8ad05fb3c659c75bc962c3eeedfe0cf59df69
egarciatom <101406426+egarciatom@users.noreply.github.com>
2022-03-18 14:28:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+function ezkerra_biratu () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+}
+function eskuina_biratu () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+}
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        ezkerra_biratu()
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        eskuina_biratu()
+    }
+})
